b21ff30f31b3f1bcf0e137456033985a06210c4a
Kong <cha5126568@naver.com>
2018-02-19 17:15:12 +0900

    인자 바로 잡는 기능 추가

--- rust_reversing_helper.py
+++ rust_reversing_helper.py
@@ -1,5 +1,5 @@
 #-*- coding: cp949 -*-
-import idc
+from idc import *
 import idautils
 import idaapi
 import subprocess
@@ -19,6 +19,13 @@ def nameMake(name):
 	name = name.replace("<","(")
 	name = name.replace(">",")")
 	return name
+
+def nameFilter(name):
+	table = "<>:()&[]"
+	for c in table:
+		name = name.replace(c,"_")
+	return name 
+
 def getUserFunctions(is_dwarf):
 	ret = []
 	if is_dwarf:
@@ -41,6 +48,7 @@ def getUserFunctions(is_dwarf):
 			for func in idautils.Functions(main_main,main_addr):
 				ret += [func]
 	return ret
+
 def setLibFunc(name,addr):
 	libNameList = ["core::","alloc::","std::","builtins::","std_unicode::"]
 	for chk in libNameList:
@@ -79,6 +87,7 @@ def demangle():
 		if full_name != old_name :
 			MakeNameEx(addr, nameMake(full_name), SN_NOCHECK | 0x800) #SN_FORCE
 			SetFunctionCmt(addr, full_name, 1)
+
 def stringRecoveryA():
 	_rodata = idaapi.get_segm_by_name(".rodata")
 	_data_rel_ro = idaapi.get_segm_by_name(".data.rel.ro")
@@ -113,10 +122,10 @@ def stringRecoveryB():
 	userfunc = getUserFunctions(False)
 	for func in userfunc:
 		addr_list = []
-		func_ = func
-		while func < FindFuncEnd(func_):
-			addr_list += [func]
-			func = FindCode(func , SEARCH_DOWN)
+		inst = func
+		while inst < FindFuncEnd(func):
+			addr_list += [inst]
+			inst = FindCode(inst , SEARCH_DOWN)
 		for i in xrange(len(addr_list)-2):
 			if GetMnem(addr_list[i]) == "lea" and GetMnem(addr_list[i+1]) == "mov" and GetMnem(addr_list[i+2]) == "mov":
 				if "qword ptr [" in GetOpnd(addr_list[i+1],0) and "qword ptr [" in GetOpnd(addr_list[i+2],0):
@@ -147,13 +156,64 @@ def stringRecoveryB():
 				 		if Cmt != "":
 				 			#print hex(addr_list[i]),Len,get_string(Qword(Addr),Len)
 				 			MakeRptCmt(addr_list[i],Cmt)
-				 		
+
+def paramRecovery():
+	#x64 fastcall in Linux
+	reg_rdi = ['rdi','edi','di','dil']
+	reg_rsi = ['rsi','esi','si','sil']
+	reg_rdx = ['rdx','edx','dx','dl']
+	reg_rcx = ['rcx','ecx','cx','cl']
+	reg_r8  = ['r8','r8d','r8w','r8b']
+	reg_r9  = ['r9','r9d','r9w','r9b']
+	regs    = [reg_rdi, reg_rsi, reg_rdx, reg_rcx, reg_r8, reg_r9]
+	bits    = [64,32,16,8]
+	dType   = ["_QWORD","_DWORD","_WORD","_BYTE"]
+	ChkDict_0 = {}
+	ChkDict_1 = {}
+	MAX_ADDR = 0xffffffffffffffff
+
+	for func in idautils.Functions():
+		pCount = 0
+		for i in xrange(0,6):
+			ChkDict_0[i] = [MAX_ADDR,""]
+			ChkDict_1[i] = [MAX_ADDR,""]
+		inst = func
+		while inst < FindFuncEnd(func):
+			if "mov" in GetMnem(inst) or "lea" in GetMnem(inst):
+				for i in xrange(len(regs)):
+					for reg in regs[i]:
+						if GetOpnd(inst,0) in reg and ChkDict_0[i][0] == MAX_ADDR:
+							ChkDict_0[i] = [inst,dType[regs[i].index(reg)]]
+						if GetOpnd(inst,1) in regs[i] and ChkDict_1[i][0] == MAX_ADDR:
+							ChkDict_1[i] = [inst,dType[regs[i].index(reg)]]
+
+			inst = FindCode(inst , SEARCH_DOWN)
+		for i in xrange(0,6):
+			if ChkDict_0[i][0] > ChkDict_1[i][0] and ChkDict_1[i][0] != MAX_ADDR:
+				pCount +=1
+		#if pCount == 6 probably uses stack
+		if pCount < 6:
+			#fType = GetType(func).split(" ")[0]
+			fName = GetFunctionName(func)
+			realType = "__int64 __fastcall " + nameFilter(fName) + " ("
+			for i in xrange(pCount):
+				if ChkDict_1[i][1] == "":
+					ChkDict_1[i][1] = "_QWORD"
+				realType += (ChkDict_1[i][1] + ",")
+			if realType[-1:] == ",":
+				realType = realType[:-1]
+			realType += ")"
+			if SetType(func,realType) != True:
+				print realType
+		
 def main():
 	demangle()
 	stringRecoveryB()
 	stringRecoveryA()
 	stringRecoveryA()
-
+	#Experimental Features(only integer argument)
+	paramRecovery()
+	
 	for addr in getUserFunctions(False):
 		print hex(addr)
 	#print getUserFunctions(False)
